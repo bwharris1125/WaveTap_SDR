93f11aa96e606731388c4abbf94ef6734e1483f5
bwharris1125 <bwharris1125@gmail.com>
2025-08-08 18:15:55 -0500

    removed windows support

--- init_env.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Windows batch script to set up Python virtual environment and install packages
-python -m venv .venv
-call .venv\Scripts\activate
-pip install --upgrade pip
-pip install mypy isort ruff pytest pre-commit
